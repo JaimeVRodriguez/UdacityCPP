e414fe295d8c839efc1dbe1380ccc9093baa61a2
JaimeVRodriguez <57820323+JaimeVRodriguez@users.noreply.github.com>
2021-02-12 15:23:35 -0500

    Vector continued

--- vectorAccess.cpp
+++ vectorAccess.cpp
@@ -4,17 +4,22 @@ using std::vector;
 using std::cout;
 
 int main() {
+    // Access individual elements of vector
     vector<int> a = {0, 1, 2, 3, 4};
     cout << a[0];
     cout << a[1];
     cout << a[2];
     cout << "\n";
 
+    // Access 2D vector
     vector<vector<int>> b = {{1, 1, 2, 3},
                              {2, 1, 2, 3},
                              {3, 1, 2, 3}};
-    
+
     cout << b[0][1];
     cout << "\n";
-}
 
+    // Print size of array
+    cout << a.size() << "\n";
+
+}
