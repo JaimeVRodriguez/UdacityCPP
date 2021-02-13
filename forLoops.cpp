633f365b1bbd6cbd3aa59d974e5ca1fa8adad473
JaimeVRodriguez <57820323+JaimeVRodriguez@users.noreply.github.com>
2021-02-12 19:51:49 -0500

    Continued for loop

--- forLoops.cpp
+++ forLoops.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
+#include <vector>
 using std::cout;
+using std::vector;
 
 int main() {
     for (int i=0; i < 5; i++) {
@@ -14,4 +16,34 @@ int main() {
     cout << "The value of c is: " << c << "\n";
     cout << "The value of i is: " << i << "\n";
     cout << "\n";
+
+
+    vector<int> v_1 = {1, 2, 3, 3, 5};
+    // Reads like
+    // for "i" in "a"
+    for (int i: v_1) {
+      cout << i << "\n";
+    }
+
+
+    /*
+    Practice
+    */
+
+    for (int i = -3; i <= 10; i++) {
+      cout << i << "\n";
+    }
+
+    vector<vector<int>> v_2 = {{1,2},
+                      {3,4},
+                      {5,6}};
+
+    for(auto i: v_2) {
+      for(auto j: i) {
+        cout << j << " ";
+      }
+      cout << "\n";
+    }
+
+
 }
